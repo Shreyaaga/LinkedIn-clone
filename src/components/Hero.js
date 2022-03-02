2d73f1b1b4d0f4af1215b8ee41faa6e15c06b37b
Shreya Agarwal <93423387+Shreyaaga@users.noreply.github.com>
2022-03-02 15:46:53 +0530

    first_commit

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <div>
+            
+                <div className="w-36 h-10">
+                    <img src="/images/image.png"/>
+                </div>
+            
+        </div>
+    );
+}
+export default Hero;
